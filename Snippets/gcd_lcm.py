f307a677de90b88b9a5510d4e22f5045fc890369
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-05-12 17:13:24 +0800

    Add Snipeets

--- Snippets/gcd_lcm.py
+++ Snippets/gcd_lcm.py
@@ -1,1 +1,8 @@
-__author__ = 'Fikri'
+def gcd(a,b):
+    (b,a) = sorted( (a,b) )
+    while b != 0:
+        (a,b) = (b, a%b)
+    return a
+
+def lcm(a,b):
+    return a*b / gcd(a,b)
--- Snippets/primes.py
+++ Snippets/primes.py
@@ -1,1 +1,12 @@
-__author__ = 'Fikri'
+def gen_primes(self):
+    D = {}
+    q = 2
+    while True:
+        if q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        q += 1
