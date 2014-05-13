c1976c5358a83741d94d3e8ae4a79092daa9f580
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-05-13 11:13:34 +0800

    SRM 615 Div 2 Problem 1-2

---
+++ SRM 615/LongLongTripDiv2.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class LongLongTripDiv2 {
+	public String isAble(long D, int T, int B) {
+		for (long i=0; i<=T; i++) {
+			if (D == i * B + (T - i)) {
+				return "Possible";
+			}
+		}
+		return "Impossible";
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("LongLongTripDiv2 (500 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            long D = Long.parseLong(Reader.nextLine());
+            int T = Integer.parseInt(Reader.nextLine());
+            int B = Integer.parseInt(Reader.nextLine());
+            Reader.nextLine();
+            String __answer = Reader.nextLine();
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(D, T, B, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1399950409;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(long D, int T, int B, String __expected) {
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		LongLongTripDiv2 instance = new LongLongTripDiv2();
+		String __result = "";
+		try {
+			__result = instance.isAble(D, T, B);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__expected.equals(__result)) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + "\"" + __expected + "\"");
+			System.err.println("           Received: " + "\"" + __result + "\"");
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "LongLongTripDiv2.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
